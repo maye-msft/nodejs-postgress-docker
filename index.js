7e247b41c6eb5d8ec9016ed92b8c09794aeae840
maye-msft <34230764+maye-msft@users.noreply.github.com>
2018-10-11 22:05:51 +0800

    change port

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ app.get('/testdb', function(req, res){
 /*
  * On server start
  */
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+
+const PORT = process.env.PORT || 5000
+
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
